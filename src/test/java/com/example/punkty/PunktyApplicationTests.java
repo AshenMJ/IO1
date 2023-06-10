e7c8bd3c534fe7a144b0ca0895b568d92f703675
Mikołaj Jucha <mikolaj.jucha@spiid.pl>
2023-06-10 16:02:12 +0200

    zad9

--- src/test/java/com/example/punkty/PunktyApplicationTests.java
+++ src/test/java/com/example/punkty/PunktyApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.punkty;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class PunktyApplicationTests {
+class Punkty1ApplicationTests {
 
 	@Test
 	void contextLoads() {
